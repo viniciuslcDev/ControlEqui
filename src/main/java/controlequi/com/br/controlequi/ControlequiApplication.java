becca25ee40943a4e36276f7e46098bc99891105
vinicius.cabral <vinicius.c.lima2004@gmail.com>
2025-04-28 12:10:03 -0300

    iniciando projeto

---
+++ src/main/java/controlequi/com/br/controlequi/ControlequiApplication.java
@@ -0,0 +1,13 @@
+package controlequi.com.br.controlequi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlequiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlequiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/controlequi/com/br/controlequi/ControlequiApplicationTests.java
@@ -0,0 +1,13 @@
+package controlequi.com.br.controlequi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlequiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
