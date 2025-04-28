a7462b5aa39f80a8bad4e08cd656f8ebdddc9c9a
vinicius.cabral <vinicius.c.lima2004@gmail.com>
2025-04-28 15:10:23 -0300

    adicionando equipamentomodel

---
+++ src/main/java/controlequi/com/br/controlequi/Model/Equipamento.java
@@ -0,0 +1,30 @@
+package controlequi.com.br.controlequi.Model;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "equipamentos")
+public class Equipamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEquipamento;
+
+    @Column(name = "data_aquisicao", nullable = false)
+    private LocalDate dataAquisicao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_Funcionario", referencedColumnName = "id_Funcionario", nullable = false)
+    private Funcionario funcionario;
+
+    @Column(name = "tipo_equipamento", nullable = false)
+    private String tipoEquipamento;
+
+    @Column(name = "status_equipamento", nullable = false)
+    private boolean statusEquipamento; // true para "em uso" e false para "em estoque"
+
+    @Column(nullable = false)
+    private int SerialNumber;
+
+}
