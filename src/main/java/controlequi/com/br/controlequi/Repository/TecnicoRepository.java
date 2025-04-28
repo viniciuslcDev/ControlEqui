eb5ef182a1fd0c8d7e141ba50460b3d05db6534e
joao.camilo <camilojoaoestevao@gmail.com>
2025-04-28 18:40:28 -0300

    feat:manutencao tecnicorepository

--- src/main/java/controlequi/com/br/controlequi/Repository/TecnicoRepository.java
+++ src/main/java/controlequi/com/br/controlequi/Repository/TecnicoRepository.java
@@ -1,9 +1,10 @@
 package controlequi.com.br.controlequi.Repository;
 
+import controlequi.com.br.controlequi.Model.TecnicoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TecnicoRepository extends JpaRepository {
+public interface TecnicoRepository extends JpaRepository<TecnicoModel, Long> {
 
 }
